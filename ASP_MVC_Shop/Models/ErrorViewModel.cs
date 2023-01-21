b53f178583192d7453aaa8ef2a0a07af26974654
petrov6525 <petrov6525@gmail.com>
2023-01-21 10:30:26 +0200

    FirstCommit

---
+++ ASP_MVC_Shop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP_MVC_Shop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ ASP_MVC_Shop/wwwroot/js/site.js
@@ -0,0 +1,73 @@
+﻿	// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+	// for details on configuring this project to bundle and minify static web assets.
+
+	// Write your JavaScript code.
+
+	$(() => {
+		if (window.scrollY > 100) {
+			$("#upButton").show();
+		}
+	});
+
+	$(".category-label").click(function (e) {
+		$(e.target).toggleClass("text-orange");
+	});
+
+
+
+	$(".menu-button").click(() => {
+		$(".menu-div").toggleClass("hide-menu-div");
+	});
+
+	function Scroll(e) {
+
+		if (window.scrollY > 100) {
+			$("#upButton").show();
+		}
+		else {
+			$("#upButton").hide();
+		};
+	};
+
+
+
+
+
+	$("#upButton").click(() => {
+		window.scrollTo({
+			top: 0,
+			behavior: "smooth"
+		});
+
+		
+	});
+
+	$(".user").click(() => {
+
+		if ($(".basket-div").hasClass("wide")) {
+			$(".basket-div").removeClass("wide");
+		}
+
+
+		if ($(".basket").hasClass("active")) {
+			$(".basket").removeClass("active");
+		}
+		$(".user").toggleClass("active");
+		$(".user-div").toggleClass("wide");
+
+	});
+
+	$(".basket").click(() => {
+
+		if ($(".user-div").hasClass("wide")) {
+			$(".user-div").removeClass("wide");
+		}
+
+
+		if ($(".user").hasClass("active")) {
+			$(".user").removeClass("active");
+		}
+		$(".basket").toggleClass("active");
+
+		$(".basket-div").toggleClass("wide");
+	});
